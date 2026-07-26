b9d01750bc48b11b08ac16a3030e542c518fdd2a
pragathinaveen <pragathid7@gmail.com>
2026-07-26 22:52:12 +0200

    new pyth0on file as old file is deleted

---
+++ CreatingWebsite_SCC.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SALT CITY CHAMPIONSHIP",
+    page_icon="🏏",
+    layout="wide"
+)
+
+# Centered Header
+st.markdown(
+    """
+    <h1 style='text-align: center;'>
+    SALT CITY CHAMPIONSHIP
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
+
+# Subtitle
+st.markdown(
+    """
+    <h3 style='text-align: center;'>
+    Match Verification Platform
+    </h3>
+    """,
+    unsafe_allow_html=True
+)
+
+# Text
+st.write("Hello! This website is created to verify matches.")
