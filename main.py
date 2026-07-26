ba5b910af39bddecd23477cd886a8d943131bd58
Pragathi Naveen <pragathinaveen@Pragathis-MacBook-Air.local>
2026-07-26 22:08:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="SALT CITY CHAMPIONSHIP", page_icon=":cricket_game:", layout="wide")
+
+#Header
+st.subheader("SALT CITY CHAMPIONSHIP")
+
+# Text
+st.write("Hello! This website is created to verify matches")
