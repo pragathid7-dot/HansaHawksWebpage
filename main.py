26b4e3c9028ef81acaf23bfa4b16bff9aaa36561
pragathinaveen <pragathid7@gmail.com>
2026-07-26 22:49:19 +0200

    moving header to center

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="SALT CITY CHAMPIONSHIP", page_icon=":cricket_game:", layout="wide")
-
-#Header
-st.subheader("SALT CITY CHAMPIONSHIP")
-
-# Text
-st.write("Hello! This website is created to verify matches")
